a17e658cb92c2d9b1e5f73db6bb61e394705f5bf
Perpetual Echelon <diamondbackspartan@gmail.com>
2025-11-09 08:42:56 -0500

    null checks consistency across SexHospitalListeners
    Resolves:
    
    ERROR com.fs.starfarer.combat.CombatMain  - java.lang.NullPointerException: Cannot invoke "com.fs.starfarer.api.campaign.CampaignFleetAPI.getFleetData()" because the return value of "com.fs.starfarer.api.campaign.SectorAPI.getPlayerFleet()" is null
    java.lang.NullPointerException: Cannot invoke "com.fs.starfarer.api.campaign.CampaignFleetAPI.getFleetData()" because the return value of "com.fs.starfarer.api.campaign.SectorAPI.getPlayerFleet()" is null
            at data.scripts.intel.SexHospitalShipListener.reportFleetMemberDeployed(SexHospitalShipListener.java:18)

--- src/data/scripts/intel/SexHospitalCampaignListener.java
+++ src/data/scripts/intel/SexHospitalCampaignListener.java
@@ -12,17 +12,20 @@ public class SexHospitalCampaignListener extends BaseCampaignEventListener {
         super(permaRegister);
     }
 
-    @Override
-    public void reportShownInteractionDialog(InteractionDialogAPI dialog) {
-        Logger l = Logger.getLogger(this.getClass().getName());
-        super.reportShownInteractionDialog(dialog);
-        float totalFleetHospitalShipBonus = 1f;
-        for(FleetMemberAPI s:Global.getSector().getPlayerFleet().getFleetData().getMembersListCopy()){
-            if(s.getVariant().hasHullMod("sex_hospital")) {
-                totalFleetHospitalShipBonus = totalFleetHospitalShipBonus * s.getStats().getDynamic().getStat("sex_global_crew_loss").getModifiedValue();
-            }
-        }
-        Global.getSector().getPlayerFleet().getStats().getDynamic().getStat("overall_crew_loss_mult").modifyMult(this.getClass().getName(), totalFleetHospitalShipBonus);
-        //l.info("crew loss mult ="+totalFleetHospitalShipBonus);
-    }
+	@Override
+	public void reportShownInteractionDialog(InteractionDialogAPI dialog) {
+		if (Global.getSector() == null || Global.getSector().getPlayerFleet() == null) return;
+
+	float totalFleetHospitalShipBonus = 1f;
+	for (FleetMemberAPI s : Global.getSector().getPlayerFleet().getFleetData().getMembersListCopy()) {
+		if (s.getVariant().hasHullMod("sex_hospital")) {
+		totalFleetHospitalShipBonus *= s.getStats().getDynamic()
+			.getStat("sex_global_crew_loss").getModifiedValue();
+		}
+	}
+
+	Global.getSector().getPlayerFleet().getStats().getDynamic()
+		.getStat("overall_crew_loss_mult")
+		.modifyMult(this.getClass().getName(), totalFleetHospitalShipBonus);
+	}
 }
